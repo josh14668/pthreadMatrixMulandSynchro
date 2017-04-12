4952162207535105d4dbe2b2d220f1589824379c
Joshua Manogaran <joshuamanogaran@Joshuas-MacBook-Pro.local>
2017-04-12 13:05:23 +1000

    finished code needs some cleaning up

--- matrixMul.c
+++ matrixMul.c
@@ -2,24 +2,128 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+typedef enum { false, true } bool;
+#define SIZE 256
+
+
 float **A,**B, **C; //global variables for two-dimensional arrays
 int M, K, N; //matrix dimensions
 int num_thrds; //number of threads to be created
+pthread_attr_t attr;
+pthread_mutex_t mutexLock;
+
+struct matrixSubSet{
+	int id;
+	int start_col;
+	int end_col;
+	//float *vector;
+
+};
+
+void populateMat(float** Mat,int row,int column,bool setZero){
+	srand48(time(NULL));
+
+	for(int i=0;i<row;i++){
+		for(int j =0;j<column;j++){
+			if(setZero){
+				Mat[i][j]=0;
+			}else{
+				//Mat[i][j]=1;
+				Mat[i][j]=drand48();
+			}
+		}
+	}
+}
+
+float **matMul(int r1, int c1, int c2, float **A, float**B){
+
+  float **matC=(float**)malloc(sizeof(float)*SIZE);
+
+	for (int i=0;i<r1;i++){
+    matC[i]=(float*)calloc(c2,sizeof(float));
+  }
+
+	for(int i=0; i<r1; ++i)
+        for(int j=0; j<c2; ++j)
+            for(int k=0; k<c1; ++k)
+            {
+                matC[i][j]+=A[i][k]*B[k][j];
+            }
+
+return matC;
+}
+
+
 
 //thread routine for matrix multiplication.
 void *mat_mult_thrds(void *threadarg)
 {
-	//local variables declaration
-	... please fill in if any.
+	struct matrixSubSet *matLims =(struct matrixSubSet*) threadarg;
+	int id = matLims->id;
+	int a = matLims->start_col;
+	int b =matLims->end_col;
+	int num_columns_temp=(b-a+1);
+
+	//Allocates space for subset
+	float **temp_ans = (float**)malloc(SIZE*K);
+	for(int i=0;i<K;i++){
+		temp_ans[i]=(float*)calloc(sizeof(float), num_columns_temp);
+	}
+//Fills Subset with column values from B
+	for(int i=0;i<K;i++){
+		for(int j=a;j<=b;j++){
+			temp_ans[i][j-a]=B[i][j];
+		}
+	}
+	float **temp_sub = (float**)malloc(SIZE*M);
+	for(int i=0;i<M;i++){
+		temp_sub[i]=calloc(sizeof(float), N);
+	}
+	temp_sub=matMul(M, K, num_columns_temp, A, temp_ans);
+
+	//pthread_mutex_lock(&mutexLock);
+
+	printf("\nMatrix subset thread%d:(%dx%d):\n",id,K,num_columns_temp);
+	printf("Thread %d limit:%dto%d\n",id,a,b);
+	printf("\n");
+	for(int i=0; i<K; i++){
+	for(int j=0; j<num_columns_temp; j++){
+		printf("%f ",temp_ans[i][j]);
+
+	}
+	printf("\n");
+	}
+
+	printf("\n");
+
+	printf("\nMatrix multiplication thread%d:(%dx%d):\n",id,M,num_columns_temp);
+	printf("Thread %d limit:%dto%d\n",id,a,b);
+	printf("\n");
+
+	for(int i=0; i<M; i++){
+	for(int j=0; j<num_columns_temp; j++){
+		printf("%f ",temp_sub[i][j]);
+
+	}
+	printf("\n");
+	}
+
+	printf("\n");
+
+
+	//pthread_mutex_unlock(&mutexLock);
+	for(int i=0;i<M;i++){
+		for(int j=a;j<=b;j++){
+			C[i][j]=temp_sub[i][j-a];
+		}
+	}
+
 
 	//retrieve my thread id from *threadarg.
-	... please fill in.
 
 	//calculate my task i.e., matrix C columns using N, num_thrds and my thread id.
-	... please fill in.
 
 	//perform my task for matrix multiplication.
-	... please fill in.
 
 	pthread_exit(NULL);
 }
@@ -28,54 +132,184 @@ int main(int argc, char *argv[])
 {
 	pthread_t *threads;
 	int *t; //user defined thread ids
-	int rc, i, j;
+	int rc,m,n,k;
 	void *status;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
 	//... add more if necessary.
 
 	// ask for matrices dimensions M, K, N for the user.
-	... please fill in.
-
+	printf("Please insert matrix dimensions m(int):\n");
+	scanf("%d",&m);
+	printf("Please insert matrix dimensions k(int):\n");
+	scanf("%d",&k);
+	printf("Please insert matrix dimensions n(int):\n");
+	scanf("%d",&n);
+	M=m;
+	K=k;
+	N=n;
 	//ask for the number of threads num_thrds to be created from the user.
-	... please fill in.
+	printf("How many threads:\n");
+	scanf("%d",&num_thrds);
+	printf("A:%dx%d and B:%dx%d\n",m,k,k,n );
+
+	int colPerThread = 0;
+  int lastThreadExtraCol = 0;
+
+  if(num_thrds>=n){
+    printf("Too many threads than necessary or same number of threads as columns\n");
+    num_thrds=n;
+  }else{
+    printf("There are %d columns\n", n);
+    printf("%d threads can handle %d columns each plus %dth thread needs to handle %d extra columns\n",num_thrds,n/num_thrds,num_thrds,n%num_thrds);
+    colPerThread = n/num_thrds;
+    lastThreadExtraCol = n%num_thrds;
+
+  }
+	printf("%d threads\n",num_thrds);
+
+
+
+	struct matrixSubSet subset[num_thrds];
 
 	//create matrices A, B and C using malloc().
-	... please fill in.
+
+	A = (float**)malloc(SIZE*m);
+	for(int i=0;i<m;i++){
+		A[i]=(float*)calloc(k,sizeof(float));
+	}
+
+	B = (float**)malloc(SIZE*k);
+	for(int i=0;i<k;i++){
+		B[i]=(float*)calloc(n,sizeof(float));
+	}
+
+	C = (float**)malloc(SIZE*m);
+	for(int i=0;i<m;i++){
+		C[i]=(float*)calloc(n,sizeof(float));
+	}
+
+
+
 
 	//initialize matrices A and B using srand() and rand()
 	//the range of float values should be between 0 and 1.0.
 	//initialize matrix C.
-	... please fill in.
+
+	populateMat(A, m, k, false);
+	populateMat(B, k, n,false);
+	//populateMat(C, m, n,true);
 
 	//allocate memory for thread ids.
-	... please fill in.
+	threads = (pthread_t*)malloc(sizeof(pthread_t)*num_thrds);
+	t=(int*)malloc(sizeof(int)*num_thrds);
 
 	//create threads.
-	for(i=0; i<num_thrds; i++) {
-		t[i] = i; //user defined thread id to be passed to the thread routine
-		rc = pthread_create(&threads[i], NULL, mat_mult_thrds, (void *) &t[i]);
-		if (rc) {
-			printf("ERROR; return code from pthread_create() is %d\n", rc);
-			exit(-1);
-		}
-	}
+	// for(i=0; i<num_thrds; i++) {
+	// 	t[i] = i; //user defined thread id to be passed to the thread routine
+	// 	rc = pthread_create(&threads[i], NULL, mat_mult_thrds, (void *) &t[i]);
+	// 	if (rc) {
+	// 		printf("ERROR; return code from pthread_create() is %d\n", rc);
+	// 		exit(-1);
+	// 	}
+	// }
+
+	int i;
+	pthread_mutex_init(&mutexLock,NULL);
+
+  if(num_thrds==n){
+    for(int i=0;i<num_thrds;i++){
+      subset[i].id=i;
+      subset[i].start_col=i;
+      subset[i].end_col=i;
+      pthread_create((threads+i),&attr,mat_mult_thrds,(void*)(subset+i));
+    }
+  }else{
+    for(i = 0; i<num_thrds-1;i++){
+      if(i==0){
+        subset[i].start_col=0;
+        subset[i].end_col =subset[i].start_col+(colPerThread-1);
+
+      }else{
+        subset[i].start_col=subset[i-1].start_col+(colPerThread);
+        subset[i].end_col =subset[i].start_col+(colPerThread-1);
+
+      }
+      subset[i].id=i;
+      printf("Thread %d starts at column:%d\n",i,subset[i].start_col);
+      printf("Thread %d ends at column:%d\n",i,subset[i].end_col);
+
+      pthread_create((threads+i),&attr,mat_mult_thrds,(void*)(subset+i));
+    }
+
+    subset[i].id=i;
+    subset[i].start_col = subset[i-1].end_col+1;
+    subset[i].end_col = subset[i].start_col+(colPerThread-1)+lastThreadExtraCol;
+    pthread_create((threads+i),&attr,mat_mult_thrds,(void*)(subset+i));
+
+    printf("Thread %d starts at column:%d\n",i,subset[i].start_col);
+    printf("Thread %d ends at column:%d\n",i,subset[i].end_col);
+}
+
+
+
+
+
+
 
 	//join threads.
-	for(i=0;i<num_thrds;i++) {
-	... please fill in.
+	for(int i=0;i<num_thrds;i++) {
+		pthread_join(threads[i], NULL);
+	}
 
+	//TODO Finish the sequnetial matrix checker
 	//check correctness -
 	//perform a sequential computation using the same A and B and
 	//compare the results.
-	... please fill in.
 
- 	printf("\n Output Matrix C:\n");
-	for(i=0; i<M; i++){
-		for(j=0; j<N; j++){
-			printf("%f  ", C[i][j]);
-			if(j == N - 1)
-				printf("\n\n");
+
+	printf("\nMatrix A(%dx%d):\n",m,k);
+	for(int i=0; i<m; i++){
+		for(int j=0; j<k; j++){
+			printf("%f  ", A[i][j]);
+
 		}
+		printf("\n");
 	}
+
+	printf("\nMatrix B(%dx%d):\n",k,n);
+	for(int i=0; i<k; i++){
+	for(int j=0; j<n; j++){
+		printf("%f  ", B[i][j]);
+
+	}
+	printf("\n");
+	}
+
+	//C=matMul(m, k, n, A, B);
+
+
+	printf("\nMatrix C Threaded(%dx%d):\n",m,n);
+	for(int i=0; i<m; i++){
+	for(int j=0; j<n; j++){
+		printf("%f  ", C[i][j]);
+
+	}
+	printf("\n");
+	}
+
+	printf("\n");
+
+	printf("\nMatrix C Serial(%dx%d):\n",m,n);
+	for(int i=0; i<m; i++){
+	for(int j=0; j<n; j++){
+		printf("%f  ", matMul(m, k, n, A, B)[i][j]);
+
+	}
+	printf("\n");
+	}
+
 
 	pthread_exit(NULL);
 }
--- synchro.c
+++ synchro.c
@@ -11,83 +11,225 @@ int no_of_terminals;
 int no_of_free_terms;
 
 //anything you want to add
-... please fill in.
 
 
 void * customer_routine(void *);
 void * attendant_routine(void *);
 
 //declare global mutex and condition variables
-... please fill in
+pthread_mutex_t no_seat_mutex;
+pthread_mutex_t no_terminals_mutex;
+
+pthread_cond_t attendent_cond;
+pthread_cond_t customer_arrives_cond;
+pthread_cond_t terminal_use_finished_cond;
+pthread_cond_t terminal_available_cond;
+
 
 
 int main(int argc, char ** argv)
 {
 	//any variables
-	... please fill in.
+	int customer_pace, terminal_usage_time;
 
 
 	// ask user to provide the total number of seats & total number of terminals.
-	... please fill in,
+	printf("Total number of seats in waiting room(int):\n");
+	scanf("%d",&no_of_seats);
+	printf("Total number of Terminals(int):\n");
+	scanf("%d",&no_of_terminals);
+
+	//Initally all seats and terminals are free
+	no_of_free_seats=no_of_seats;
+	no_of_free_terms=no_of_terminals;
 
 
 	// ask user to provide the total number of customers, customers arrival rate & terminal usage time.
-	... please fill in.
+	printf("Total Number of customers(int):\n");
+	scanf("%d",&no_of_customers);
+	printf("Customers arrival rate(int):\n");
+	scanf("%d",&customer_pace);
+	printf("Terminal usage time (int):\n");
+	scanf("%d",&terminal_usage_time);
+
 
 
 	//Initialize mutexes and condition variable objects
-	... please fill in.
+	pthread_mutex_init(&no_seat_mutex, NULL);
+	pthread_mutex_init(&no_terminals_mutex, NULL);
+
+	pthread_cond_init(&customer_arrives_cond, NULL);
+	pthread_cond_init(&attendent_cond, NULL);
+	pthread_cond_init(&terminal_use_finished_cond, NULL);
+	pthread_cond_init(&terminal_available_cond, NULL);
+
 
 
 	// anything you want to add
-	... please fill in.
 
+	pthread_t *thread = (pthread_t*)malloc((no_of_customers+1) * sizeof(pthread_t)); //total is No_Of_Consuers + 1 to include farmer
+
+	if(thread == NULL){
+		fprintf(stderr, "threads out of memory\n");
+		exit(1);
+	}
+	int * t_ids = malloc((no_of_customers+1) * sizeof(int)); //total is No_Of_Consuers + 1 to include farmer
+	if(t_ids == NULL){
+		fprintf(stderr, "t out of memory\n");
+		exit(1);
+	}
 
 	//create the attendant thread.
-	... please fill in.
-
+	int rc = pthread_create(&thread[0],NULL,attendant_routine,NULL);
+	if (rc) {
+		printf("ERROR; return code from pthread_create() (attendant) is %d\n", rc);
+		exit(-1);
+	}
 
 	//create consumer threads according to the arrival rate (in the range between 0 and arrival rate) and
 	//pass user-defined id and terminal usage (in the range between 0 and terminal usage)
-	... please fill in.
+	srand(time(0));
 
+	for (int k = 1; k<=no_of_customers; k++)
+	{
+		sleep((int)rand() % customer_pace); //sleep a few second before creating a thread
+		t_ids[k] = k;
+		int rc = pthread_create(&thread[k], NULL, customer_routine, (void *)&t_ids[k]); //consumer routine takes thread id as the arg
+		if (rc) {
+			printf("ERROR; return code from pthread_create() (consumer) is %d\n", rc);
+			exit(-1);
+		}
+	}
 
 	//anything you want to add
-	... please fill in.
+
+	for(int i=1;i<=no_of_customers;i++){
+		pthread_join(thread[i],NULL);
+	}
+
+	pthread_cancel(thread[0]);
 
 
-    pthread_exit(NULL);
+	free(thread);
+	free(t_ids);
+
+  pthread_exit(EXIT_SUCCESS);
+
+
+
 }
 
 void * attendant_routine(void * noargs)
 {
+	//TODO: What if all seats are filled?
     while (1) //Continue to serve customers.
     {
-		//The attendant thread must print the following status messages wherever appropriate:
-		//"Attendant: The number of free seats is %d. No customers and I'm waiting. \n"
-		//"Attendant: The number of free seats now is %d. try to find a free terminal.\n"
-		//"Attendant: The number of free terminal s is %d. All terminals are occupied. \n"
-		//"Attendant: The number of free terminals is %d. There are free terminals now. \n"
-		//"Attendant: Call one customer. The number of free seats is now %d.\n"
-		//"Attendant: Assign one terminal to the customer. The number of free terminals is now %d.\n"
+			pthread_mutex_lock(&no_seat_mutex);
+			if(no_of_free_seats==no_of_seats){
+				//Attendant is waiting on customers, as there are no customer currently in the waiting room
+				printf("Attendant: The number of free seats is %d. No customers and I'm waiting. \n",no_of_free_seats);
+				pthread_cond_wait(&customer_arrives_cond, &no_seat_mutex);
+				pthread_mutex_unlock(&no_seat_mutex);
+			}else{
 
-        ... please fill in.
+				printf("Attendant: The number of free seats now is %d. trying to find a free terminal\n",no_of_free_seats);
 
+				//No Terminals available
+				pthread_mutex_lock(&no_terminals_mutex);
+				if(no_of_free_terms==0){
+					printf("Attendant: The number of free terminals is %d. All terminals are occupied. ", no_of_free_terms);
+					//Waiting for terminals to become free
+					pthread_cond_wait(&terminal_use_finished_cond,&no_terminals_mutex);
+					pthread_mutex_unlock(&no_terminals_mutex);
+				}
+				printf("Attendant: The number of free terminals is %d. There are free terminals now. \n", no_of_free_terms);
 
+				no_of_free_seats++;
+				no_of_free_terms--;
+				pthread_mutex_unlock(&no_seat_mutex);
+				pthread_mutex_unlock(&no_terminals_mutex);
+				//Signal Customer that a terminal is available
+				pthread_cond_signal(&terminal_available_cond);
+
+				printf("Attendant: Call one customer. The number of free seats is now %d\n",no_of_free_seats);
+				//pthread_mutex_unlock(&no_seat_mutex);
+				printf("Attendant: Assign one terminal to the customer. The number of free terminals is now %d\n",no_of_free_terms);
+
+			//The attendant thread must print the following status messages wherever appropriate:
+			//"Attendant: The number of free seats is %d. No customers and I'm waiting. \n"
+			//"Attendant: The number of free seats now is %d. try to find a free terminal.\n"
+			//"Attendant: The number of free terminal s is %d. All terminals are occupied. \n"
+			//"Attendant: The number of free terminals is %d. There are free terminals now. \n"
+			//"Attendant: Call one customer. The number of free seats is now %d.\n"
+			//"Attendant: Assign one terminal to the customer. The number of free terminals is now %d.\n"
+
+			//pthread_exit(NULL);
+
+			}
     }
 }
 
 void * customer_routine(void * args)
 {
+	int id =*(int*)args;
+	printf("Customer %d arrives.\n",id);
+	pthread_mutex_lock(&no_seat_mutex);
+	if(no_of_free_seats==0){
+		//If there are no free seats in waiting room customer leaves
+		printf("Customer %d oh no! all seats have been taken and I'll leave now!\n",id);
+		pthread_mutex_unlock(&no_seat_mutex);
+		pthread_exit(NULL);
+	}else{
+
+//Customer has arrived in waiting room and fills a single seat
+////////////////////////////////////////////////////////////////////////
+		//Customer takes a seat must signal to attendant that he has arrived
+		printf("Customer %d: I'm lucky to get a free seat from %d.\n",id,no_of_free_seats);
+		no_of_free_seats--;
+		pthread_mutex_unlock(&no_seat_mutex);
+		//Customer Signals to attendant that he has arrived
+		pthread_cond_signal(&customer_arrives_cond);
+////////////////////////////////////////////////////////////////////////
+
+
+//Customer is waiting to be served by the attendant
+////////////////////////////////////////////////////////////////////////
+		pthread_mutex_lock(&no_terminals_mutex);
+		//Customer must wait for signal from attendant
+		printf("Customer %d: I'm waiting to be served\n",id);
+		//pthread_mutex_lock(&no_terminals_mutex);
+		pthread_cond_wait(&terminal_available_cond,&no_terminals_mutex);
+		printf("Customer %d: I'm getting a terminal now\n",id);
+		//Customer vacates seat in waiting area and fills one terminal spot
+		//no_of_free_terms--;
+		//pthread_mutex_unlock(&no_terminals_mutex);
+		//Customer is using the terminal waits for specefied amount of time
+		sleep((int)rand() % 5);
+		printf("Customer %d: I'm finished using the terminal and leaving\n",id);
+		//pthread_mutex_lock(&no_terminals_mutex);
+
+		no_of_free_terms++;
+
+		printf("thread %d num_terms:%d\n",id,no_of_free_terms);
+		pthread_mutex_unlock(&no_terminals_mutex);
+
+		pthread_cond_signal(&terminal_use_finished_cond);
+		//Signal to attendent that customer has finished using terminal
+
+
+		//TODO: This pthread_exit statment is an issue
+		//pthread_exit(NULL);
+
+	}
+
 	//customer thread must print the following status messages wherever appropriate where id is the identifying number of the thread:
-	//"Customer %d arrives.\n"
 	//"Customer %d: oh no! all seats have been taken and I'll leave now!\n"
 	//"Customer %d: I'm lucky to get a free seat from %d.\n"
 	//"Customer %d: I'm to be served.\n"
 	//"Customer %d: I'm getting a terminal now.\n"
 	//"Customer %d: I'm finished using the terminal and leaving.\n"
 
-	... please fill in.
 
 
+	pthread_exit(NULL);
 }
